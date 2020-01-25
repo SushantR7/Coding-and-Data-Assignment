3b6e8c1d9429c485695f8a73e1aa41f8180c4119
Sushant Ramesh <sushantr@usc.edu>
2020-01-25 07:49:48 -0800

    Add files via upload

---
+++ DataAnalysis_Q1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data=pd.read_csv('SalesData.csv',skiprows=1,
+                 names=['UID','Date','AveragePrice','Total Volume','SKU #4046','SKU #4225','SKU #4770','Total Bags','Small Bags','Large Bags','XLarge Bags','category','year','region'
+])  
+
+data['month'] = pd.DatetimeIndex(df['Date']).month
+
+x=data.groupby(['Region','month').sum()  #Displays region and month with total volume summed up according to the aforementioned parameters
+
+s = x[x['Total Volume']>1500000000]  #Data with total volume greater than $1.5 billion
